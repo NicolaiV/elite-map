83e3572915bbd4585a42d9c864614f664af33290
ns.voevodin <ns.voevodin@tensor.ru>
2017-04-13 18:39:14 +0300

    Рефакторинг

--- datas.js
+++ datas.js
@@ -1,134 +1,133 @@
-var http = require('https')
-var fs = require('fs')
-var eachOfLimit = require('async').eachOfLimit;
-var mongoose = require('mongoose');
-var db = null;
-	 mongoose.Promise = global.Promise;
-	 
-var System = mongoose.model('System', 
+const http = require('https')
+const fs = require('fs')
+const eachOfLimit = require('async').eachOfLimit;
+const bluebird = require('bluebird')
+const mongoose = require('mongoose')
+
+mongoose.Promise = bluebird;
+   
+const connect = mongoose.connect('mongodb://localhost:27017/')
+let db = mongoose.connection.db;
+
+const System = mongoose.model('System', 
 {  
-	id: Number,
-	edsm_id: Number,
-	name: String,
-	x: Number,
-	y: Number,
-	z: Number 
+  id: Number,
+  edsm_id: Number,
+  name: String,
+  x: Number,
+  y: Number,
+  z: Number 
 });
 
-function downloadFile(url, path, callback){	
-	var stream = new fs.createWriteStream(path);
-	http.get(url, function (res) {
-		console.log(`Загрузка ${url}`);
-		let strJson = '';
-		res.on('data', function (chunk) {
-			stream.write(chunk);
-		});
-		res.on('end', function () {
-			console.log(`Запись ${url} в ${path}`);
-			stream.end()
-			fs.readFile(path, (code, data)=>{
-				if(code){
-					callback(e);
-				} else {
-					try{
-						JSON.parse(data.toString())
-					}
-					catch(e) {
-						downloadFile(url, path, callback);
-					}
-					callback();
-				}
-			});
-		});
-		res.on('error', (e) => {
-			callback(e);
-		});
-	})
+function downloadFile(url, path, callback){  
+  let stream = new fs.createWriteStream(path);
+  http.get(url, function (res) {
+    console.log(`Загрузка ${url}`);
+    let strJson = '';
+    res.on('data', function (chunk) {
+      stream.write(chunk);
+    });
+    res.on('end', function () {
+      console.log(`Запись ${url} в ${path}`);
+      stream.end()
+      fs.readFile(path, (code, data)=>{
+        if(code){
+          callback(e);
+        } else {
+          try{
+            JSON.parse(data.toString())
+          }
+          catch(e) {
+            downloadFile(url, path, callback);
+          }
+          callback();
+        }
+      });
+    });
+    res.on('error', (e) => {
+      callback(e);
+    });
+  })
 }
 
 function getFile(url, path, callback){
-	try{
-		fs.stat(path, (err, stat) => {
-			const date = new Date();
-			if( stat
-				&& (stat.ctime.getFullYear() === date.getFullYear())
-				&& (stat.ctime.getMonth() === date.getMonth())
-				&& (stat.ctime.getDate() === date.getDate())){
-					callback();
-				} else {
-					downloadFile(url, path, callback)		
-				}
-		})
-	} catch (err){
-		callback(err)
-	}
+  try{
+    fs.stat(path, (err, stat) => {
+      const date = new Date();
+      if( stat
+        && (stat.ctime.getFullYear() === date.getFullYear())
+        && (stat.ctime.getMonth() === date.getMonth())
+        && (stat.ctime.getDate() === date.getDate())){
+          callback();
+        } else {
+          downloadFile(url, path, callback)    
+        }
+    })
+  } catch (err){
+    callback(err)
+  }
 }
 
 function getJsonPath(item){
-	return `data/${item}.json`
+  return `data/${item}.json`  
 }
 
 function getAPI(api, done){
-	try {
-		eachOfLimit(api, 1, (value, key, callback) => getFile(value, getJsonPath(key), (code)=>{if(code){done(code)} callback()}), () => done(0, Object.keys(api).map( item => getJsonPath(item))))
-	} catch (err){
-		done(err)
-	}
+  try {
+    eachOfLimit(api, 1, (value, key, callback) => getFile(value, getJsonPath(key), (code)=>{if(code){done(code)} callback()}), () => done(0, Object.keys(api).map( item => getJsonPath(item))))
+  } catch (err){
+    done(err)
+  }
 }
 
 function updateDB() {
-	return new Promise(function(resolve, reject) {
-		var pathToSystemsJSON = getJsonPath('systems');
-		getFile("https://eddb.io/archive/v5/systems_populated.json", pathToSystemsJSON, (err, data)=>{
-			if(err){
-				console.log('Ошибка чтения файла')
-				reject(err);
-			}
-			console.log('Файл прочитан')
-			var systems = JSON.parse(fs.readFileSync(pathToSystemsJSON).toString());
-			console.log(systems.length)
-			eachOfLimit(systems, 1, (value, key, callback) => {
-					let system = new System(value);
-					system.save((err, data, affected) => callback());
-					}, () =>{console.log('END'); 
-					System.count({}, function(err, c) {
-					   console.log('Count is ' + c);
-					});
-					resolve()});
-			delete systems;
-		})
-	});
+  return new Promise(function(resolve, reject) {
+    const pathToSystemsJSON = getJsonPath('systems');
+    getFile("https://eddb.io/archive/v5/systems_populated.json", pathToSystemsJSON, (err, data)=>{
+      if(err){
+        console.log('Ошибка чтения файла')
+        reject(err);
+      }
+      console.log('Файл прочитан')
+      fs.readFile(pathToSystemsJSON, (code, data)=>{
+        if(code){
+          reject(e);
+        } else {
+          systems = JSON.parse(data.toString());
+          console.log(systems.length)
+          eachOfLimit(systems, 1, (value, key, callback) => {
+            let system = new System(value);
+            system.save((err, data, affected) => callback());
+          }, () =>{console.log('END'); 
+          System.count({}, function(err, c) {
+            console.log('Count is ' + c);
+          });
+          resolve()});
+          delete systems;
+        }
+      });
+    })
+  });
 }
 
-function initDB(callback){
-	return new Promise(function(resolve, reject) {
-		console.log('initDB');
-		mongoose.connect('mongodb://localhost:27017/');
-		db = mongoose.connection.db;
-		mongoose.connection.on('open', (err) => err ? reject() : resolve());
-	})
+function closeDB(){
+  return new Promise(function(resolve) {
+    console.log('closeDB');
+    mongoose.connection.close(resolve);
+  })
 }
 
-function clearDB(callback){
-	return new Promise(function(resolve, reject) {
-		console.log('clearDB');
-		db.dropDatabase((err) => err ? reject() : resolve());
-	})
-}
-
-function closeDB(callback){
-	return new Promise(function(resolve) {
-		console.log('closeDB');
-		mongoose.connection.close(resolve);
-	})
+function getDb() { 
+  return connect
+    .then(() => connect.connection.db); 
 }
 
 module.exports = {
-	getAPI: getAPI,
-	getJsonPath: getJsonPath,
-	getFile: getFile,
-	updateDB: updateDB,
-	initDB: initDB,
-	clearDB: clearDB,
-	closeDB: closeDB
+  getAPI: getAPI,
+  getJsonPath: getJsonPath,
+  getFile: getFile,
+  updateDB: updateDB,
+  closeDB: closeDB,
+  db: db,
+  getDb: getDb
 }
--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-var datasGetter = require('./datas');
-var fs = require('fs');
-var eachOfLimit = require('async').eachOfLimit;
+const datasGetter = require('./datas');
+const fs = require('fs');
+const eachOfLimit = require('async').eachOfLimit;
 
 function distance(initial, target){
-	var {x: initialX, y: initialY, z: initialZ} = initial;
-	var {x: targetX, y: targetY, z: targetZ} = target;
-	return Math.sqrt(Math.pow((initialX - targetX), 2) + Math.pow((initialY - targetY), 2 ) + Math.pow((initialZ - targetZ), 2))
+  const {x: initialX, y: initialY, z: initialZ} = initial;
+  const {x: targetX, y: targetY, z: targetZ} = target;
+  return Math.sqrt(Math.pow((initialX - targetX), 2) + Math.pow((initialY - targetY), 2 ) + Math.pow((initialZ - targetZ), 2))
 }
-
-datasGetter.initDB()
-	.then(datasGetter.clearDB)
-	.then(datasGetter.updateDB)
-	.then(datasGetter.closeDB)
-	.catch(err=>console.log(err))
-	
-	
-	/*for(let index in systems){
-	if(systems.hasOwnProperty(index)){
-		let system = systems[index];
-		console.log(index)
-	/*
-	if(systems.hasOwnProperty(index)){
-		let system = systems[index];
-		system.distances = [];
-		for(let targetIndex in systems){
-			if(systems.hasOwnProperty(targetIndex)){
-				let target = systems[index];
-				if(target.distances && target.distances[index]){
-					system.distances[targetIndex] = target.distances[index];
-				}
-				system.distances[targetIndex] = distance(system, target);
-			}
-		}
-	}*//*
-	}
+datasGetter.getDb()
+  .then(db => db.dropDatabase())
+  .then(datasGetter.updateDB)
+  .then(datasGetter.closeDB)
+  .catch(err=>console.log(err))
+  
+  
+  /*for(let index in systems){
+  if(systems.hasOwnProperty(index)){
+    let system = systems[index];
+    console.log(index)
+  /*
+  if(systems.hasOwnProperty(index)){
+    let system = systems[index];
+    system.distances = [];
+    for(let targetIndex in systems){
+      if(systems.hasOwnProperty(targetIndex)){
+        let target = systems[index];
+        if(target.distances && target.distances[index]){
+          system.distances[targetIndex] = target.distances[index];
+        }
+        system.distances[targetIndex] = distance(system, target);
+      }
+    }
+  }*//*
+  }
 }*/
+
+//TODO: подключить линтер
