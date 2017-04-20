59db4f1cafb5a4018626e7f624cc9bf05320552c
ns.voevodin <ns.voevodin@tensor.ru>
2017-04-20 17:48:15 +0300

    Запись в БД расстояний между системами

--- index.js
+++ index.js
@@ -9,32 +9,34 @@ const schedule = require('node-schedule');
   2. Файл парсится и перебирается итерационно
   3. Для каждой системы в БД высчитываются расстояния до всех систем, до кторых расстояний нет
 5. Всё работает
-
-function distance(initial, target){
-  const {x: initialX, y: initialY, z: initialZ} = initial;
-  const {x: targetX, y: targetY, z: targetZ} = target;
-  return Math.sqrt(Math.pow((initialX - targetX), 2) + Math.pow((initialY - targetY), 2 ) + Math.pow((initialZ - targetZ), 2))
-}*/
+*/
 
 let updaeterErrorDeep = 0;
 
 function reActualData() {
   console.log('reActualData');
   return mongoInterface.initDb()
-   // .then(() => mongoInterface.db.dropDatabase())
+    .then(() => mongoInterface.db.dropDatabase())
     .then(() => mongoInterface.System.count({}))
     .then((count) => {
       console.log(`Count of systems is ${count}`);
       return count;
     })
+    .then(() => mongoInterface.Distance.count({}))
+    .then((count) => {
+      console.log(`Count of distance is ${count}`);
+      return count;
+    })
     .then(count => mongoInterface.actualDB(count === 0))
    // .then(()=>{}) //Обойти базу данных и дописать списки путей там, где их нет
    // .then(mongoInterface.closeDB)
-    .then(() => { updaeterErrorDeep = 0; })
+    .then(() => { console.log('END'); updaeterErrorDeep = 0; })
     .catch((err) => {
-      if (updaeterErrorDeep < 10) {
+		
+      console.log(`err` + err);
+      if (updaeterErrorDeep < 5) {
         updaeterErrorDeep++;
-        setTimeout(reActualData, updaeterErrorDeep * 12000);
+        setTimeout(reActualData, Math.pow(updaeterErrorDeep, 3)  * 1000);
       } else {
         throw err;
       }
--- mongo_interface.js
+++ mongo_interface.js
@@ -4,7 +4,7 @@ const fsp = require('fs-es6-promise');
 const downloader = require('./downloader');
 
 const pathToSystemsJSON = downloader.getJsonPath('systems');
-
+Promise = bluebird;
 mongoose.Promise = bluebird;
 
 const connect = mongoose.connect('mongodb://localhost:27017/elite');
@@ -22,6 +22,7 @@ const systemSchema = new mongoose.Schema({
   y: Number,
   z: Number,
   population: Number,
+  distances: Array
   /*is_populated: Boolean,
   government_id: Number,
   government: String,
@@ -47,6 +48,21 @@ const systemSchema = new mongoose.Schema({
 
 const System = mongoose.model('System', systemSchema);
 
+
+function distance(initial, target){
+  const {x: initialX, y: initialY, z: initialZ} = initial;
+  const {x: targetX, y: targetY, z: targetZ} = target;
+  return Math.sqrt(Math.pow((initialX - targetX), 2) + Math.pow((initialY - targetY), 2 ) + Math.pow((initialZ - targetZ), 2))
+}
+
+const distanceSchema = new mongoose.Schema({
+  docAId: Number,
+  docBId: Number,
+  dist: Number
+});
+
+const Distance = mongoose.model('Distance', distanceSchema);
+
 function closeDB() {
   console.log('closeDB');
   return mongoose.connection.close();
@@ -63,7 +79,31 @@ function updateDB() {
     .then((data) => {
       const systems = JSON.parse(data.toString());
       return bluebird.map(systems, system => new System(system).save());
-    });
+    })
+    .then(()=> new Promise((resolve) => {
+      System.find({}, function (err, docs) {
+        let dists = [];
+        return Promise.each(docs, (itemA, indexA) => {
+            console.log(indexA);
+            return Promise.each(docs, (itemB, indexB) => {
+               if(indexB > indexA){
+                   dists.push({
+                   docAId: itemA.id,
+                   docBId: itemB.id,
+                   dist: distance(itemA, itemB)
+                 });
+               if(dists.length > 100000){
+                 return Distance.collection.insert(dists)
+                   .then(() => { dists = []; });
+               }
+               }
+            });
+         })
+		   .then(() => Distance.collection.insert(dists)
+						.then(() => { dists = []; }));
+       })
+    })
+  )
 }
 
 function actualDB(force) {
@@ -93,5 +133,6 @@ module.exports = {
   closeDB,
   db,
   initDb,
-  System
+  System,
+  Distance
 };
