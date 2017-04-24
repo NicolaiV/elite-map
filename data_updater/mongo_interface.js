74d3f69814cd9db974a962ff1dbcd9397d0358d3
Nikolai <nicolai.v.95@gmail.com>
2017-04-24 23:06:40 +0300

    построчное чтение файла

--- data_updater/mongo_interface.js
+++ data_updater/mongo_interface.js
@@ -1,10 +1,13 @@
 const bluebird = require('bluebird');
 const mongoose = require('mongoose');
 const fsp = require('fs-es6-promise');
+const fs = require('fs');
+const readline = require('readline');
 const downloader = require('./downloader');
 const System = require('../DB_Models/System');
 const Distance = require('../DB_Models/Distance');
 const config = require('./config');
+const Stream = require('stream');
 
 const pathToSystemsJSON = downloader.getJsonPath('systems');
 Promise = bluebird;
@@ -36,11 +39,32 @@ function initDb() {
 //TODO: сделать построчное чтение файла
 function updateDB() {
   return downloader.downloadFile(config.systemsUrl, pathToSystemsJSON)
-    .then(() => fsp.readFile(pathToSystemsJSON))
-    .then((data) => {
-      const systems = data.toString().split('\n').filter( item => item !== '');
-      return bluebird.map(systems, system => new System(JSON.parse(system)).save());
-    })
+    .then(() => new Promise((resolve, reject) => {
+       const instream = fs.createReadStream(pathToSystemsJSON);
+       const outstream = new Stream();
+       outstream.readable = true;
+       outstream.writable = true;
+       let systems = [];
+       let rl = readline.createInterface({
+          input: instream,
+          output: outstream
+       })
+       .on('line', function(line) {
+          try {
+            systems.push(JSON.parse(line));
+          } catch (e){}
+          if(systems.length > 1000) {
+            System.collection.insert(systems)
+            systems = [];
+          }
+        })
+       .on('error', err => reject(err))
+       .on('close', () => {
+          System.collection.insert(systems)
+          systems = [];
+          resolve();
+        });
+    }))
     .then(() => new Promise((resolve) => {
       System.find({}, (err, docs) => {
         let dists = [];
@@ -51,9 +75,9 @@ function updateDB() {
             if (indexB > indexA && d < config.maxRadius) {
               dists.push({
                 names: [itemA.name,  itemB.name],
-				X: [itemA.x, itemB.x],
-				Y: [itemA.y, itemB.y],
-				Z: [itemA.z, itemB.z],
+                X: [itemA.x, itemB.x],
+                Y: [itemA.y, itemB.y],
+                Z: [itemA.z, itemB.z],
                 distance: d
               });
               if (dists.length > config.mongoose.size) {
